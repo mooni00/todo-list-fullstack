0e43e62b390eb842a8bb0f998837c6cd4fe4696e
Mxuuni <iratni.mounira00@gmail.com>
2026-04-20 19:24:06 +0200

    Configure GitHub Pages deployment and CI/CD

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/todo-list-fullstack/', // Set the base path for the application to ensure correct asset loading when deployed to GitHub Pages
 })
